88fa3c424f15de55bd2eed1f9ebd92d61a87f59c
c.kayomb <c.kayomb@groupeonepoint.com>
2024-04-01 23:45:33 +0200

    changes updated

--- src/main/java/com/lasuperbe/cicdpipeline/cicdpipeline/web/TestController.java
+++ src/main/java/com/lasuperbe/cicdpipeline/cicdpipeline/web/TestController.java
@@ -8,11 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping(path = "/test")
+//@RequestMapping(path = "/test")
 public class TestController {
 
-    @GetMapping
+    @GetMapping("/test")
     public ResponseEntity<Map<String, String>> test(){
         return ResponseEntity.ok().body(Map.of("Testing", "Up and running"));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Map<String, String>> healthcheck(){
+        return ResponseEntity.ok().body(Map.of("Status", "Up"));
     }
 }
